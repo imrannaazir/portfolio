0db3a6510bcb49fb6e397ac19bb77b7ffbd7e4dc
unknown <imran nazir>
2022-06-07 00:18:25 +0600

    used framer motion and font awesome

--- src/Portfolio/Home/Service.jsx
+++ src/Portfolio/Home/Service.jsx
@@ -1,12 +1,31 @@
 import React from "react";
-
+import { motion } from "framer-motion";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faRunning } from "@fortawesome/free-solid-svg-icons";
 const Service = () => {
   return (
-    <div className="flex flex-col justify-center items-center w-[250px] bg-base-200 px-8 py-8 text-center gap-4 rounded-md">
-      <img
-        class="mask mask-hexagon hover:mask-hexagon-2 transform duration-1000 w-24"
-        src="https://api.lorem.space/image/shoes?w=160&h=160"
-      />
+    <div className="group flex flex-col justify-center items-center w-[250px] bg-base-200 px-8 py-8 text-center gap-4 rounded-md ">
+      <div className="relative">
+        <motion.div
+          whileHover={{
+            scale: 1.2,
+            transition: { duration: 0.3 },
+            rotate: 90,
+            backgroundColor: "#30A5BF",
+          }}
+          whileTap={{
+            scale: 0.8,
+            rotate: -90,
+            borderRadius: "100%",
+          }}
+          class="bg-base-300 group-hover:  mask mask-hexagon transform h-24 w-24 relative"
+        ></motion.div>
+        <FontAwesomeIcon
+          className="absolute inset-6 text-5xl"
+          icon={faRunning}
+        />
+      </div>
+
       <h3 className="text-lg">Hello</h3>
       <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
     </div>
